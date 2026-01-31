03a69e84167d0c46f92eaaa873dc9c23d915d339
omrai2505-ind <omrai2505@gmail.com>
2026-01-31 22:47:22 +0530

    commit 5 retry

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
   string command;
   while(getline(cin , command)){
     if (command.substr(0,5) == "type "){
-        if (command.substr(5) == "echo" || command.substr(5) == "exit") cout<<command.substr(5)<<" is a shell builtin"<<endl;
+        if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type") cout<<command.substr(5)<<" is a shell builtin"<<endl;
         else cout<<command.substr(5)<<": not found"<<endl;
         cout<<"$ ";
         continue;
