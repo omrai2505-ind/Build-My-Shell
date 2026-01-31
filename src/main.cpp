8ba93afb449043d4568fbba14117972b2bc1e6a2
omrai2505-ind <omrai2505@gmail.com>
2026-01-31 20:05:32 +0530

    commit 4

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,9 @@ int main() {
   cout << "$ ";
   string command;
   while(getline(cin , command)){
-    if (command == "exit"){
-      break;
-    }
-    cout<<command<<": command not found"<<endl;
+    if (command == "exit") exit(0);
+    if (command.substr(0,5) == "echo ") cout<<command.substr(5,command.length())<<endl;
+    else cout<<command<<": command not found"<<endl;
     cout<<"$ ";
   }
   
---
+++ src/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+
+
+  cout << "$ ";
+  string command;
