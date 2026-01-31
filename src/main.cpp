401bc0f30eb0148521b3da801b8444c9c8915f98
omrai2505-ind <omrai2505@gmail.com>
2026-01-31 13:14:27 +0530

    Commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
+  string command;
+  getline(cin , command);
+  cout<<command<<" : command not found!";
 }
